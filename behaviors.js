42f80bbba2e81033f5e39f7e8ccfcad183e5ef25
Brad Czerniak <ao5357@gmail.com>
2012-01-23 13:58:53 -0500

    Switched the calculation process from a function to a jQuery method. It should allow chaining, as well as multiple calculators per page. Intermediate transition state

--- behaviors.js
+++ behaviors.js
@@ -1,35 +1,35 @@
-$(document).ready(function(){
-/* Styling */
-$("#totalResult").css({'color':'#000','text-align':'center'});
-
-/* Functions */
-function roicalculate(){
-	var roiTotal = 0; // Grand total
-	$("#calculator tbody tr").each(function(i){
-		var outVal = 0; // Row total
-		var obj = $(this);
-		var firstInput = obj.find("td:first input");
-		if(firstInput.attr("type") == "number" || firstInput.attr("type") == "text"){
-			var inVal = new String(firstInput.val());
-			inVal = inVal.replace(/[^0-9]/g,''); // remove non-numeric characters before evaluating
-			if(inVal !== ''){
-				var outVal = eval(inVal + firstInput.attr("data-roi-formula"));
+(function($){
+$.fn.calcROI = function(){  
+	return this.each(function(){
+		var $this = $(this);
+		var roiTotal = 0; // Grand total
+		$this.find("tbody tr").each(function(i){
+			var obj = $(this);
+			var outVal = 0; // Row total
+			var firstInput = obj.find("td:first input");
+			if(firstInput.attr("type") == "number" || firstInput.attr("type") == "text"){
+				var inVal = new String(firstInput.val());
+				inVal = inVal.replace(/[^0-9]/g,''); // remove non-numeric characters before evaluating
+				if(inVal !== ''){
+					var outVal = eval(inVal + firstInput.attr("data-roi-formula"));
+					}
 				}
-			}
-		else if(firstInput.attr("type") == "checkbox"){
-			if(firstInput.filter("input:checked").length == 1){
-				var outVal = eval(1 + firstInput.attr("data-roi-formula"));
+			else if(firstInput.attr("type") == "checkbox"){
+				if(firstInput.filter("input:checked").length == 1){
+					var outVal = eval(1 + firstInput.attr("data-roi-formula"));
+					}
 				}
-			}
-		// Further else ifs for validation based on other input types can go here
-		outVal = parseFloat(outVal).toFixed(2);
-		obj.find("td:last input").val(outVal);
-		roiTotal += parseFloat(outVal);
+			// Further else ifs for validation based on other input types can go here
+			outVal = parseFloat(outVal).toFixed(2);
+			obj.find("td:last input").val(outVal);
+			roiTotal += parseFloat(outVal);
+			});
+		$this.find("input[disabled]:last").val(roiTotal.toFixed(2));
 		});
-	$("#totalResult").val(roiTotal.toFixed(2));
-	}
+	};
+})(jQuery);
 
-/* Events */
+$(document).ready(function(){
 $("#calculator tbody tr").find("td:eq(1) label").each(function(){
 	var label = $(this);
 	if(label.siblings("p.roi-explanation").length == 1){
@@ -50,15 +50,12 @@ $("#calculator tbody tr").find("td:eq(1) label").each(function(){
 		label.css({'padding-left':'19px'});
 		}
 	});
-
-$("#calculator").submit(function(){ // calculate when someone hits 'Enter' and make sure the form doesn't actually submit
-	roicalculate();
+$(".calculator").submit(function(){ // calculate when someone hits 'Enter' and make sure the form doesn't actually submit
+	$(this).calcROI();
 	return false;
 	});
-$("input").change(function(){ // replace the onChange event attributes with bindings
-	roicalculate();
-	}).keyup(function(){ // use the keyup event to make the app more responsive
-	roicalculate();
+$(".calculator").on("change keyup","input",function(e){
+	$(this).parents(".calculator").calcROI();
 	});
-roicalculate(); // Calculate on page load just in case
+$(".calculator").calcROI(); // Calculate on page load just in case
 });
