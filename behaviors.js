be496628a8d3cb26c1c1fa85784bdce4bfd93111
ao5357 <ao5357@gmail.com>
2012-01-23 22:10:42 -0500

    chained some lines and declarations

--- behaviors.js
+++ behaviors.js
@@ -2,20 +2,17 @@
 (function($){
 $.fn.calcROI = function(){ // requires rather specific conditions
 	return this.each(function(){
-		var $this = $(this);
-		var totalROI = 0;
+		var $this = $(this), totalROI = 0;
 		$this.find("tbody tr").each(function(i){
-			var row = $(this);var inVal = 0;var outVal = 0;
+			var row = $(this), inVal = 0, outVal = 0;
 			var input = row.find("input");
 			if(input.attr("type") == "number" || input.attr("type") == "text"){
-				inVal = new String(input.val());
-				inVal = inVal.replace(/[^0-9]/g,''); // remove non-numeric characters before evaluating
+				inVal = new String(input.val()).replace(/[^0-9\.]/g,''); // remove non-numeric characters before evaluating
 				inVal = (inVal.length >= 1) ? inVal : 0;
 				}
 			else if(input.attr("type") == "checkbox" && input.filter("input:checked").length == 1){
 				inVal = 1;
-				}
-			// Further else ifs for validation based on other input types can go here
+				} // validate other input types in subsequent else ifs
 			var formula = (input.attr("data-roi-formula")) ? input.attr("data-roi-formula") : '';
 			outVal = eval(inVal + formula);
 			outVal = parseFloat(outVal).toFixed(2);
@@ -29,21 +26,18 @@ $.fn.calcROI = function(){ // requires rather specific conditions
 
 document.createElement("output"); // shiv for IE8-; Before jQ traverses the DOM
 $(document).ready(function(){ // bind events
-$(".calculator").calcROI().find("tbody tr").find("label").each(function(){
-	var label = $(this);
-	if(label.siblings(".roi-explanation").length >= 1){
-		label.siblings(".roi-explanation").hide();
-		label.addClass('helptext').attr("title","Click to toggle explanatory text").click(function(e){
-			label.siblings(".roi-explanation").slideToggle("slow");
-			$("#" + label.attr("for")).focus().select();
-			e.preventDefault();
-			});
-		}
-	});
-$(".calculator").submit(function(){
-	$(this).calcROI();
-	return false;
-	}).on("change keyup input","input",function(e){
-		$(this).parents(".calculator").calcROI();
-	});
+$(".calculator").calcROI()
+	.submit(function(){$(this).calcROI();return false;})
+	.on("change keyup input","input",function(e){$(this).parents(".calculator").calcROI();})
+	.find("tbody tr").find("label").each(function(){
+		var label = $(this);
+		if(label.siblings(".roi-explanation").length >= 1){
+			label.siblings(".roi-explanation").hide();
+			label.addClass('helptext').attr("title","Click to toggle explanatory text").click(function(e){
+				label.siblings(".roi-explanation").slideToggle("slow");
+				$("#" + label.attr("for")).focus().select();
+				e.preventDefault();
+				});
+			}
+		});
 });
